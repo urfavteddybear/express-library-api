1aa2f3c7cbfbac39bbe8994b3469cc64584c120b
unknown <business.urfavteddybear@gmail.com>
2024-08-18 16:02:41 +0800

    fix: idk what i do wrong lol

--- routes/categories.js
+++ routes/categories.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('../database.db');
+const db = new sqlite3.Database('./database.db');
 
 // GET all categories
 router.get('/', (req, res) => {
