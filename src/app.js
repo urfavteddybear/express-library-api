fdcaa1a82f1c4edbd8ee75796d7a95722b5e9da3
unknown <business.urfavteddybear@gmail.com>
2024-09-29 14:39:32 +0800

    feat: advance logging

--- src/app.js
+++ src/app.js
@@ -3,14 +3,23 @@ const bodyParser = require('body-parser');
 const app = express();
 const booksRouter = require('./routes/books');
 const categoriesRouter = require('./routes/categories');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 
 // Logging middleware
-app.use((req, res, next) => {
-    const now = new Date();
-    const logMessage = `${now.toISOString()} - ${req.method} ${req.originalUrl}`;
-    console.log(logMessage);
-    next(); // Proceed to the next middleware or route handler
-});
+// app.use((req, res, next) => {
+//     const now = new Date();
+//     const logMessage = `${now.toISOString()} - ${req.method} ${req.originalUrl}`;
+//     console.log(logMessage);
+//     next(); // Proceed to the next middleware or route handler
+// });
+
+// Logging
+const accessLogStream = fs.createWriteStream(path.join(__dirname, '../access.log'), { flags: 'a' });
+app.use(morgan(':date[iso] - :remote-addr - :method :url :status :response-time ms', { stream: accessLogStream })); // Log to file
+app.use(morgan(':date[iso] - :remote-addr - :method :url :status :response-time ms')); // Log to console as well
+app.set('trust proxy', true);
 
 app.use(bodyParser.json());
 
