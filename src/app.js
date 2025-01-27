17859288a625300b5a4cfc5c58f933e67c3e0b15
urfavteddybear <business.urfavteddybear@gmail.com>
2025-01-27 22:49:46 +0800

    chore: removing unused code

--- src/app.js
+++ src/app.js
@@ -7,14 +7,6 @@ const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
 
-// Logging middleware
-// app.use((req, res, next) => {
-//     const now = new Date();
-//     const logMessage = `${now.toISOString()} - ${req.method} ${req.originalUrl}`;
-//     console.log(logMessage);
-//     next(); // Proceed to the next middleware or route handler
-// });
-
 // Logging
 const accessLogStream = fs.createWriteStream(path.join(__dirname, '../access.log'), { flags: 'a' });
 app.use(morgan(':date[iso] - :remote-addr - :method :url :status :response-time ms', { stream: accessLogStream })); // Log to file
